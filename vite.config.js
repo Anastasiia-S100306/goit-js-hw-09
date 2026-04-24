ad9874a9eb7208d04a37a29568279d8cb805b059
Anastasiia <anastasiia@Anastasiia-MacBook-Air.local>
2026-04-24 18:32:25 +0300

    fix and update navigation

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,17 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/goit-js-hw-09/', 
+  base: '/goit-js-hw-09/',
   root: 'src',
   build: {
+    outDir: '../dist', 
+    emptyOutDir: true, 
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
         gallery: resolve(__dirname, 'src/1-gallery.html'),
         form: resolve(__dirname, 'src/2-form.html'),
       },
-      outDir: 'dist',
     },
   },
 });
